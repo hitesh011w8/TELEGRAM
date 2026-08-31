1571f1927b321db1f2d86de2faca235528d66124
hitesh011w8 <hiteshsen935173@gmail.com>
2026-08-31 09:38:27 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,74 @@
+import os
+from os import environ
+
+# API Configuration
+API_ID = int(os.environ.get("API_ID", "22750400"))
+API_HASH = os.environ.get("API_HASH", "a0213f361ff290f021156c8dad30195b")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8555750789:AAElENN8JpvpqMe7_Ty52zTfIYIhPnhsmRs")
+
+CREDIT = os.environ.get("CREDIT", "𝐌 𝐈 𝐂 𝐇 𝐀 𝐄 𝐋")
+# MongoDB Configuration
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "CpprivateApi")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://itsgoluAPI:jrMHSipToKUEnmcp@cpprivateapi.ghhp3oz.mongodb.net/?appName=CpprivateApi")  # Add your own atlas db
+MONGO_URL = DATABASE_URL  # For auth system
+
+# Owner and Admin Configuration
+OWNER_ID = int(os.environ.get("OWNER_ID", "8500762588"))
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "8500762588").split()]  # Default to owner ID
+
+# Channel Configuration
+PREMIUM_CHANNEL = ""
+# Thumbnail Configuration
+THUMBNAILS = list(map(str, os.environ.get("THUMBNAILS", "https://i.ibb.co/WvvYDZkK/5afec16d5b7e.jpg").split())) # Image Link For Default Thumbnail 
+
+# Web Server Configuration
+WEB_SERVER = os.environ.get("WEB_SERVER", "False").lower() == "true"
+WEBHOOK = True  # Don't change this
+PORT = int(os.environ.get("PORT", 8000))
+
+# Message Formats
+AUTH_MESSAGES = {
+    "subscription_active": """<b>🎉 Subscription Activated!</b>
+
+<blockquote>Your subscription has been activated and will expire on {expiry_date}.
+You can now use the bot!</blockquote>\n\n Type /start to start uploading """,
+
+    "subscription_expired": """<b>⚠️ Your Subscription Has Ended</b>
+
+<blockquote>Your access to the bot has been revoked as your subscription period has expired.
+Please contact the admin to renew your subscription.</blockquote>""",
+
+    "user_added": """<b>✅ User Added Successfully!</b>
+
+<blockquote>👤 Name: {name}
+🆔 User ID: {user_id}
+📅 Expiry: {expiry_date}</blockquote>""",
+
+    "user_removed": """<b>✅ User Removed Successfully!</b>
+
+<blockquote>User ID {user_id} has been removed from authorized users.</blockquote>""",
+
+    "access_denied": """<b>⚠️ Access Denied!</b>
+
+<blockquote>You are not authorized to use this bot.
+Please contact the admin to get access.</blockquote>""",
+
+    "not_admin": "⚠️ You are not authorized to use this command!",
+    
+    "invalid_format": """❌ <b>Invalid Format!</b>
+
+<blockquote>Use format: {format}</blockquote>"""
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
